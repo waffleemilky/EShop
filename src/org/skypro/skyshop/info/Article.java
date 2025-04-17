1f033551a75d9aba4afe079d715bd8f4dabf1e90
akilika <angelikaakinina05@gmail.com>
2025-04-17 18:43:39 +0300

    Set

--- src/org/skypro/skyshop/info/Article.java
+++ src/org/skypro/skyshop/info/Article.java
@@ -1,5 +1,7 @@
 package org.skypro.skyshop.info;
 
+import java.util.Objects;
+
 public final class Article implements Searchable {
     private final String title;
     private final String text;
@@ -32,5 +34,17 @@ public final class Article implements Searchable {
     @Override
     public String getName() {
         return getTitle();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return Objects.equals(title, article.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(title);
     }
 }
